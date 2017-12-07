ac9578f3dffc350db62acf66f0d7e10255e6a5a6
Nicolae <nastas_colea@yahoo.com>
2017-12-07 15:47:48 -0500

    nnastas created PolygonUtils class with method to calculate the sum of elements in an Array

--- BuildingAgency/src/main/java/com/building/agency/utils/array/ArrayUtils.java
+++ BuildingAgency/src/main/java/com/building/agency/utils/array/ArrayUtils.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.building.agency.utils;
+package com.building.agency.utils.array;
 
 /**
  * Declare the ArrayUtils Class
