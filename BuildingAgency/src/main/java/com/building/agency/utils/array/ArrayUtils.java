3026db793fca042eafab7da1fa6a2954c98e194f
Nicolae <nastas_colea@yahoo.com>
2017-12-21 14:58:29 -0500

    NNASTAS changed the extract data methods

--- BuildingAgency/src/main/java/com/building/agency/utils/array/ArrayUtils.java
+++ BuildingAgency/src/main/java/com/building/agency/utils/array/ArrayUtils.java
@@ -5,33 +5,38 @@
  */
 package com.building.agency.utils.array;
 
+import java.lang.reflect.Array;
+import java.util.Comparator;
+
 /**
  * Declare the ArrayUtils Class
  * @author nnastas
  * @version 1.0
+ * @param <A>
  */
-public abstract class ArrayUtils {
-
+public abstract class ArrayUtils<A> {
+    
     private DataStorage data = null; // data storage is assigned here
     
-    public abstract void onSuccess(double data);// method calls in cases of success 
-    public abstract void onFailure(double data);// method calls in case of failure
+    public abstract void onSuccess(A data);// method calls in cases of success 
+    public abstract void onFailure(A data);// method calls in case of failure
     
     /**
      * Declare the constructor for ArrayUtils 
+     * @param clazz contains the data of type A
      * @param capacity of the Array length
      * @version 1.0
      */
-    public ArrayUtils(int capacity){
+    public ArrayUtils(Class<A> clazz, int capacity){
         
-        data = new DataStorage(capacity);// creates data storage
+        data = new DataStorage<>(clazz, capacity);// creates data storage
     }
     /**
      * method to add data
      * @param data to be added
      * @version 1.0
      */
-    public void addData(double data) {
+    public void addData(A data) {
         if (this.data.addData(data)) {
            onSuccess(data);//calls the onSuccess class
         } else 
@@ -43,7 +48,7 @@ public abstract class ArrayUtils {
      * @param data looked up for
      * @version 1.0
      */
-    public void findData(double data) {
+    public void findData(A data) {
         if(this.data.findData(data) != -1){
             onSuccess(data);
         } else
@@ -56,12 +61,26 @@ public abstract class ArrayUtils {
      * @return the value based on index
      * @version 1.0
      */
-    public double findData(int index) {
-        double value = 0;
+    public A findData(int index) {
+        A value = null;
         if(index >=0) {
-           value = this.data.findData(index);
+           value = (A)this.data.findData(index);
         } 
      return value;       
+    }
+    
+    /**
+     * method to find the index based on the data
+     * @param data that is stored
+     * @return the index for the data
+     * @version 1.0
+     */
+    public int findIndex(A data) {
+        int index = 0;
+        if(data != null) {
+            index = this.data.findData(data);
+        }
+        return index;
     }
     
     /**
@@ -89,9 +108,22 @@ public abstract class ArrayUtils {
      * @param newData changes the old data
      * @version 1.0
      */
-    public void changData(double data, double newData) {
+    public void changeData(A data, A newData) {
         if(this.data.changeData(data,newData)){
            onSuccess(newData); 
+        } else
+            onFailure(newData);
+    }
+    
+    /**
+     * method to change data by index
+     * @param index wher the data is located
+     * @param newData changes the old data
+     * @version 1.0
+     */
+    public void changeData(int index, A newData) {
+        if(this.data.changeData(index, newData)) {
+            onSuccess(newData);
         } else
             onFailure(newData);
     }
@@ -101,7 +133,7 @@ public abstract class ArrayUtils {
      * @param data to be removed
      * @version 1.0
      */
-    public void removeData(double data) {
+    public void removeData(A data) {
         if(this.data.removeData(data)) {
             onSuccess(data);
         } else
@@ -109,24 +141,50 @@ public abstract class ArrayUtils {
     }
     
     /**
+     * method to remove data by index
+     * @param index where the data is located
+     * @version 1.0
+     */
+    public void removeData(int index) {
+        if(this.data.removeData(index)) {
+            onSuccess((A)data);
+        } else
+            onFailure((A)data);
+    }
+    
+    /**
+     * method to sort the data
+     * @param comparator is a function from java.util that compares two arguments
+     * @version 1.0
+     */
+    public void sort(Comparator <A> comparator){
+       this.data.sort(comparator);
+       
+           
+      
+    }      
+
+    
+    /**
      * Declare the Data Storage of incoming data
      * @version 1.0
      */
    
-    protected static class DataStorage {
-        private double[] data;// Data Sorage data is stored here
+    protected static class DataStorage<B> {
+        private B[] data;// Data Sorage data is stored here
         private int count;//how much data is in the Array
         private final int capacity;//maximum Arrays that can be added
         
         /**
          * Default constructor for Data Storage
+         * @param clazz - is the information of parameterized type of Array element
          * @param capacity array capacity
          * @version 1.0
          */
-        public DataStorage(int capacity){
+        public DataStorage(Class<B> clazz, int capacity){
             this.capacity = capacity;
             this.count = 0;
-            data = new double[capacity];//creates a new Array
+            data = (B[])Array.newInstance(clazz, capacity);//creates an Array with B type data and capacity
         }
        
         /**
@@ -135,7 +193,7 @@ public abstract class ArrayUtils {
          * @return return either the data was added or not
          * @version 1.0
          */
-        public boolean addData(double data){
+        public boolean addData(B data){
             boolean added = false;
             if (count < capacity) {
                this.data[count++] = data;
@@ -150,7 +208,7 @@ public abstract class ArrayUtils {
          * @return the place where the data is located
          * @version 1.0
          */
-        public int findData(double data) {
+        public int findData(B data) {
             int place = -1;
             for (int index = 0; index < count; index++){
                 if (data==this.data[index]) {
@@ -167,8 +225,8 @@ public abstract class ArrayUtils {
          * @return the value based on the index
          * @version 1.0
          */
-        public double findData(int index) {
-            double value = 0;
+        public B findData(int index) {
+            B value = null;
             if ((index < capacity) && (index >= 0)) {
                value = data[index];
             }
@@ -200,7 +258,7 @@ public abstract class ArrayUtils {
          * @return either the data was changed or not
          * @version 1.0
          */
-        public boolean changeData(double data, double newData) {
+        public boolean changeData(B data, B newData) {
             int change = findData(data);
             boolean added = false;
                 if(change != -1){
@@ -212,7 +270,24 @@ public abstract class ArrayUtils {
         }
         
         /**
-         * moves the NaN value to the end of the Array
+         * Method to change one data with another by index
+         * @param index where the data is located
+         * @param newData replaces the old data
+         * @return either the data was changed or not
+         * @version 1.0
+         */
+        public boolean changeData(int index,B newData) {
+           
+            boolean added = false;
+                if((index > 0) || (index <= capacity)){
+                    this.data[index] = newData;
+                    
+                    added = true;
+                }
+                return added;
+        }
+        /**
+         * moves the null value to the end of the Array
          * @version 1.0
          */
         private void drop() {
@@ -220,34 +295,72 @@ public abstract class ArrayUtils {
             int items = 0;
             //shift all null elements to the end
             for (int j=0; j<data.length; j++){
-                  if (data[j]==Double.NaN){
+                  if (data[j]==null){
                     for (int k=j+1; k<data.length; k++){
                     data[k-1] = data[k];
                     }
-                data[data.length-1] = Double.NaN;
-                //Double.NaN is equivalent to null but can be used with doubls
+                data[data.length-1] = null;               
                 break;
                 }
             }            
         }
         
         /**
-         * Removes data from the array by replacing it with NaN
-         * Double.NaN is equivalent to null but can be used with doubles
+         * Removes data from the array by replacing it with null
          * @param data to be removed
          * @return either the data is or not removed
          * @version 1.0
          */
-        public boolean removeData(double data) {
+        public boolean removeData(B data) {
             int remove = findData(data);
             boolean removed = false;
                 if(remove != -1) {
-                    this.data[remove]= Double.NaN;
+                    this.data[remove]= null;
                     removed = true;
                  }
             drop();
             count--;
             return removed;
         }
+        /**
+         * Replaces data by index replacing it with null
+         * @param index where the data is locate
+         * @return either the data was removed or not
+         * @version 1.0
+         */
+        public boolean removeData(int index) {
+            boolean removed = false;
+            if((index > 0) || (index <= capacity)) {
+                this.data[index] = null;
+                removed = true;
+            }
+            drop();
+            count--;
+            return removed;
+        }
+        /**
+         * Sorts the data  by comparing them
+         * @param comparator is a function from java.util that compares two arguments
+         * @version 1.0
+         */
+         public void sort(Comparator<B> comparator){
+             if(comparator != null){
+                for (int i=0; i<data.length; i++){
+                    for(int j=0; j<data.length; j++) {
+                        if (comparator.compare(data[i], data[j]) == 1){
+                        B k = data[j]; 
+                        data[j]=data[i];
+                        data[i] = k;                    
+                        break;
+                        }
+                    }
+                }
+            }
+        }
+        
+        
     }
+    
+    
+    
 }
