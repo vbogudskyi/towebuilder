81e4ef95a65e4e0033bbd016aa6e951f96d9314a
Nicolae <nastas_colea@yahoo.com>
2017-12-22 11:09:05 -0500

    nnastas	added Math3dUtils class

---
+++ BuildingAgency/src/main/java/com/building/agency/utils/Math3dUtils.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.building.agency.utils;
+
+/**
+ *
+ * @author colea
+ */
+public class Math3dUtils {
+    
+    
+    
+    
+    
+    
+    
+    //Calculate surface Areas of all 3d figures
+    //(getAntiprismSurfaceArea(Antiprism antiprism|| Parallelepiped parallelepiped))
+    //(getAntiprismVolume(Antiprism antiprism|| Parallelepiped parallelepiped))
+    //validate the 3d figure
+    //for all figures
+}
